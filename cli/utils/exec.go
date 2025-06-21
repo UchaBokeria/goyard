8ffd44626be529c29480f346e23ebe9cd967122b
UchaBokeria <ucha1bokeria@gmail.com>
2025-06-21 19:51:23 +0400

    fx

--- cli/utils/exec.go
+++ cli/utils/exec.go
@@ -7,6 +7,7 @@ import (
 )
 
 func Exec(name string, args ...string) {
+	fmt.Printf("Running command: %s %v\n", name, args)
 	cmd := exec.Command(name, args...)
 
 	// Get stdout pipe
@@ -15,7 +16,17 @@ func Exec(name string, args ...string) {
 		fmt.Printf("Error getting stdout pipe for %s: %v\n", name, err)
 		return
 	}
-
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		fmt.Printf("Error getting stderr pipe: %v\n", err)
+		return
+	}
+	go func() {
+		scanner := bufio.NewScanner(stderr)
+		for scanner.Scan() {
+			fmt.Printf("[%s][ERR] %s\n", name, scanner.Text())
+		}
+	}()
 	// Start the command
 	if err := cmd.Start(); err != nil {
 		fmt.Printf("Error starting %s: %v\n", name, err)
