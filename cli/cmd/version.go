c012c7e462c839e044c9be241678e639b7b02ccc
UchaBokeria <ucha1bokeria@gmail.com>
2025-06-21 20:00:34 +0400

    fx

--- cli/cmd/version.go
+++ cli/cmd/version.go
@@ -9,7 +9,7 @@ import (
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
-	Use:   "versionZ",
+	Use:   "version",
 	Short: "Display the version of GoYard",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("GoYard v%s\n", goyard.Version)
